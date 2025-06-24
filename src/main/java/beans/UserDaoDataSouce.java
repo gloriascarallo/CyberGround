95aaccd867f8d2699e8fb852e00472d0b3720bc9
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-24 22:19:20 +0200

    Create UserDaoDataSouce.java

---
+++ src/main/java/beans/UserDaoDataSouce.java
@@ -0,0 +1,164 @@
+package beans;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class UserDaoDataSouce implements IBeanDao<User> {
+
+	private static DataSource ds;
+
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/storage");
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+	}
+
+	private static final String TABLE_NAME = "user";
+
+	@Override
+	public synchronized void doSave(User user) throws SQLException {
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String insertSQL = "INSERT INTO " + UserDaoDataSouce.TABLE_NAME
+				+ " (ID) VALUES (?)";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setInt(1, user.getId());
+		
+			
+
+			preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+
+	@Override
+	public synchronized User doRetrieveByKey(Object o_id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		int id=(Integer)o_id;
+
+		User bean = new User();
+
+		String selectSQL = "SELECT * FROM " + UserDaoDataSouce.TABLE_NAME + " WHERE ID = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setInt(1, id);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				bean.setId(rs.getInt("ID"));
+		
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return bean;
+	}
+
+	@Override
+	public synchronized boolean doDelete(Object o_id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+        int id=(Integer)o_id;
+        
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + UserDaoDataSouce.TABLE_NAME + " WHERE ID = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setInt(1, id);
+
+			result = preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
+	@Override
+	public synchronized Collection<User> doRetrieveAll(String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		Collection<User> users = new LinkedList<User>();
+
+		String selectSQL = "SELECT * FROM " + UserDaoDataSouce.TABLE_NAME;
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				User bean = new User();
+
+				bean.setId(rs.getInt("ID"));
+				users.add(bean);
+				
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return users;
+	}
+
+
+}
