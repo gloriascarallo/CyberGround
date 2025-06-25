1a10511739115fe7c4385daefdcad251d643dfa3
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-25 11:10:30 +0200

    Create RegisteredUser_has_addressDaoDataSource.java

---
+++ src/main/java/beans/RegisteredUser_has_addressDaoDataSource.java
@@ -0,0 +1,169 @@
+package beans;
+
+	import java.sql.Connection;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.util.Collection;
+	import java.util.LinkedList;
+	import javax.naming.Context;
+	import javax.naming.InitialContext;
+	import javax.naming.NamingException;
+	import javax.sql.DataSource;
+
+	public class RegisteredUser_has_addressDaoDataSource implements IBeanDao<RegisteredUser_has_address> {
+
+		private static DataSource ds;
+
+		static {
+			try {
+				Context initCtx = new InitialContext();
+				Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+				ds = (DataSource) envCtx.lookup("jdbc/storage");
+
+			} catch (NamingException e) {
+				System.out.println("Error:" + e.getMessage());
+			}
+		}
+
+		private static final String TABLE_NAME = "registereduser_has_address";
+
+		@Override
+		public synchronized void doSave(RegisteredUser_has_address registereduser_has_address) throws SQLException {
+
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+
+			String insertSQL = "INSERT INTO " + RegisteredUser_has_addressDaoDataSource.TABLE_NAME
+					+ " (ID_HAS_ADDRESS, USERNAMEREGISTEREDUSER, NAMEADDRESS) VALUES (?, ?, ?)";
+
+			try {
+				connection = ds.getConnection();
+				preparedStatement = connection.prepareStatement(insertSQL);
+				preparedStatement.setInt(1, registereduser_has_address.getId_has_address());
+				preparedStatement.setString(2, registereduser_has_address.getUsernameRegisteredUser());
+				preparedStatement.setString(3, registereduser_has_address.getNameAddress());
+				
+				
+
+				preparedStatement.executeUpdate();
+
+			} finally {
+				try {
+					if (preparedStatement != null)
+						preparedStatement.close();
+				} finally {
+					if (connection != null)
+						connection.close();
+				}
+			}
+		}
+
+		@Override
+		public synchronized RegisteredUser_has_address doRetrieveByKey(Object o_id) throws SQLException {
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+	        int id=(Integer)o_id;
+	        
+			RegisteredUser_has_address bean = new RegisteredUser_has_address();
+
+			String selectSQL = "SELECT * FROM " + RegisteredUser_has_addressDaoDataSource.TABLE_NAME + " WHERE ID_HAS_ADDRESS = ?";
+
+			try {
+				connection = ds.getConnection();
+				preparedStatement = connection.prepareStatement(selectSQL);
+				preparedStatement.setInt(1, id);
+
+				ResultSet rs = preparedStatement.executeQuery();
+
+				while (rs.next()) {
+					bean.setId_has_address(rs.getInt("ID_HAS_ADDRESS"));
+					bean.setUsernameRegisteredUser(rs.getString("USERNAMEREGISTEREDUSER"));
+					bean.setNameAddress(rs.getString("NAMEADDRESS"));
+			}
+
+			} finally {
+				try {
+					if (preparedStatement != null)
+						preparedStatement.close();
+				} finally {
+					if (connection != null)
+						connection.close();
+				}
+			}
+			return bean;
+		}
+
+		@Override
+		public synchronized boolean doDelete(Object o_id) throws SQLException {
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+	        int id=(Integer)o_id;
+	        
+			int result = 0;
+
+			String deleteSQL = "DELETE FROM " + RegisteredUser_has_addressDaoDataSource.TABLE_NAME + " WHERE ID_HAS_ADDRESS = ?";
+
+			try {
+				connection = ds.getConnection();
+				preparedStatement = connection.prepareStatement(deleteSQL);
+				preparedStatement.setInt(1, id);
+
+				result = preparedStatement.executeUpdate();
+
+			} finally {
+				try {
+					if (preparedStatement != null)
+						preparedStatement.close();
+				} finally {
+					if (connection != null)
+						connection.close();
+				}
+			}
+			return (result != 0);
+		}
+
+		@Override
+		public synchronized Collection<RegisteredUser_has_address> doRetrieveAll(String order) throws SQLException {
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+
+			Collection<RegisteredUser_has_address> registeredusers_have_addresses = new LinkedList<RegisteredUser_has_address>();
+
+			String selectSQL = "SELECT * FROM " + RegisteredUser_has_addressDaoDataSource.TABLE_NAME;
+
+			if (order != null && !order.equals("")) {
+				selectSQL += " ORDER BY " + order;
+			}
+
+			try {
+				connection = ds.getConnection();
+				preparedStatement = connection.prepareStatement(selectSQL);
+
+				ResultSet rs = preparedStatement.executeQuery();
+
+				while (rs.next()) {
+					RegisteredUser_has_address bean = new RegisteredUser_has_address();
+
+					bean.setId_has_address(rs.getInt("ID_HAS_ADDRESS"));
+					bean.setUsernameRegisteredUser(rs.getString("USERNAMEREGISTEREDUSER"));
+					bean.setNameAddress(rs.getString("NAMEADDRESS"));
+					registeredusers_have_addresses.add(bean);
+				}
+
+			} finally {
+				try {
+					if (preparedStatement != null)
+						preparedStatement.close();
+				} finally {
+					if (connection != null)
+						connection.close();
+				}
+			}
+			return registeredusers_have_addresses;
+		}
+
+
+
+}
