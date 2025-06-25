5ee007e05b5c6ce84695dce6879e4ab477993a4f
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-25 17:33:55 +0200

    Update Add_payment_method.java

--- src/main/java/control/Add_payment_method.java
+++ src/main/java/control/Add_payment_method.java
@@ -1,11 +1,15 @@
 package control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import beans.RegisteredUser_has_method_payment;
+import beans.RegisteredUser_has_method_paymentDaoDataSource;
 
 /**
  * Servlet implementation class Add_payment_method
@@ -26,9 +30,39 @@ public class Add_payment_method extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String username=(String)request.getSession().getAttribute("username");
+		String pan=request.getParameter("PAN");
+		String expirationDate=request.getParameter("Scadenza");
+		String cvc=request.getParameter("CVC");
+		
+		
+		RequestDispatcher dispatcherToAdd_payment_methodPage=request.getRequestDispatcher("add_payment_method.jsp");
+		RegisteredUser_has_method_payment registereduser_has_method_payment=new RegisteredUser_has_method_payment();
+		RegisteredUser_has_method_paymentDaoDataSource ds=new RegisteredUser_has_method_paymentDaoDataSource();
+		
+		
+		registereduser_has_method_payment.setUsernameRegisteredUser(username);
+		registereduser_has_method_payment.setPan(pan);
+		registereduser_has_method_payment.setExpirationDate(expirationDate);
+		registereduser_has_method_payment.setCvc(cvc);
+		
+		try {
+			
+			ds.doSave(registereduser_has_method_payment);
+			
+		}
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+			dispatcherToAdd_payment_methodPage.forward(request, response);
+		}
+		
+		response.sendRedirect("payment_page.jsp");
+		
 	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
