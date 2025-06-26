c77cdb3f7b15310ea6f49761540fb89a3bcde750
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-26 12:25:18 +0200

    Update storage.sql

--- db/storage.sql
+++ db/storage.sql
@@ -3,14 +3,13 @@ CREATE DATABASE storage;
 USE storage;
 
 
-DROP TABLE IF EXISTS user;
+
 
 CREATE TABLE `user` (
   `id` int NOT NULL,
   PRIMARY KEY (`id`)
-)
+);
 
-DROP TABLE IF EXISTS registereduser;
 
 
 CREATE TABLE `registereduser` (
@@ -25,9 +24,9 @@ CREATE TABLE `registereduser` (
   UNIQUE(`username`, `idUser`),
   KEY `idUser_idx` (`idUser`),
   CONSTRAINT `idUser_RegistratedUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-)
+);
 
-DROP TABLE IF EXISTS admin;
+
 
 CREATE TABLE `admin` (
   `id` int NOT NULL,
@@ -37,55 +36,20 @@ CREATE TABLE `admin` (
   KEY `id_Admin_idx` (`id`),
   CONSTRAINT `idUser_Admin` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `username_Admin` FOREIGN KEY (`username`) REFERENCES `registereduser` (`username`) ON DELETE RESTRICT ON UPDATE CASCADE
-)
+);
 
+CREATE TABLE `category` (
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`name`)
+);
 
-DROP TABLE IF EXISTS cart;
 
 CREATE TABLE `cart` (
   `idCart` int NOT NULL,
   PRIMARY KEY (`idCart`),
   CONSTRAINT `idCart_Cart` FOREIGN KEY (`idCart`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-)
+);
 
-
-DROP TABLE IF EXISTS product_situatedin_cart;
-
-CREATE TABLE `product_situatedin_cart` (
-  `id_SituatedIn` int AUTO_INCREMENT PRIMARY KEY,
-  `idCart` int NOT NULL,
-  `idProduct` int NOT NULL,
-  `dateAdded` date NOT NULL,
-  `quantity` int NOT NULL,
-  UNIQUE (`idProduct`,`idCart`),
-  KEY `idCart_idx` (`idCart`),
-  KEY `idProduct_idx` (`idProduct`),
-  CONSTRAINT `idCart_SituatedIn` FOREIGN KEY (`idCart`) REFERENCES `cart` (`idCart`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `idProduct_SituatedIn` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-
-DROP TABLE IF EXISTS category;
-
-CREATE TABLE `category` (
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`name`)
-)
-
-DROP TABLE IF EXISTS order;
-
-CREATE TABLE `order` (
-  `id_Order` int AUTO_INCREMENT PRIMARY KEY,
-  `id` int NOT NULL,
-  `datePurchase` date DEFAULT NULL,
-  `dateDelivery` date DEFAULT NULL,
-  `dateShipping` date DEFAULT NULL,
-  `idCart` int NOT NULL,
-  UNIQUE (`id`,`idCart`),
-  KEY `idCart_idx` (`idCart`),
-  CONSTRAINT `idCart_Order` FOREIGN KEY (`idCart`) REFERENCES `cart` (`idCart`) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-
-DROP TABLE IF EXISTS product;
 
 CREATE TABLE `product` (
   `id` int NOT NULL,
@@ -98,10 +62,42 @@ CREATE TABLE `product` (
   PRIMARY KEY (`id`),
   KEY `categoryName_idx` (`categoryName`),
   CONSTRAINT `categoryName` FOREIGN KEY (`categoryName`) REFERENCES `category` (`name`) ON DELETE RESTRICT ON UPDATE CASCADE
-) 
+) ;
+
+CREATE TABLE `product_situatedin_cart` (
+  `id_SituatedIn` int AUTO_INCREMENT PRIMARY KEY,
+  `idCart` int NOT NULL,
+  `idProduct` int NOT NULL,
+  `dateAdded` date NOT NULL,
+  `quantity` int NOT NULL,
+  UNIQUE (`idProduct`,`idCart`),
+  KEY `idCart_idx` (`idCart`),
+  KEY `idProduct_idx` (`idProduct`),
+  CONSTRAINT `idCart_SituatedIn` FOREIGN KEY (`idCart`) REFERENCES `cart` (`idCart`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `idProduct_SituatedIn` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+);
 
 
-DROP TABLE IF EXISTS method_payment;
+
+
+
+
+CREATE TABLE `order` (
+  `id_Order` int AUTO_INCREMENT PRIMARY KEY,
+  `id` int NOT NULL,
+  `datePurchase` date DEFAULT NULL,
+  `dateDelivery` date DEFAULT NULL,
+  `dateShipping` date DEFAULT NULL,
+  `idCart` int NOT NULL,
+  UNIQUE (`id`,`idCart`),
+  KEY `idCart_idx` (`idCart`),
+  CONSTRAINT `idCart_Order` FOREIGN KEY (`idCart`) REFERENCES `cart` (`idCart`) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+
+
+
 
 CREATE TABLE `method_payment` (
   `pan` char(19) NOT NULL,
@@ -110,10 +106,10 @@ CREATE TABLE `method_payment` (
   PRIMARY KEY (`pan`),
   KEY `SECONDARY` (`expirationDate`),
   KEY `TERTIARY` (`cvc`)
-) 
+) ;
 
 
-DROP TABLE IF EXISTS registereduser_has_method_payment;
+
 
 CREATE TABLE `registereduser_has_method_payment` (
 
@@ -130,17 +126,17 @@ CREATE TABLE `registereduser_has_method_payment` (
   CONSTRAINT `expirationDate_HasMethodPayment` FOREIGN KEY (`expirationDateMethodPayment`) REFERENCES `method_payment` (`expirationDate`) ON UPDATE CASCADE,
   CONSTRAINT `usernameRegisteredUser_HasMethodPayment` FOREIGN KEY (`usernameRegisteredUser`) REFERENCES `registereduser` (`username`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `pan_HasMethodPayment` FOREIGN KEY (`panMethodPayment`) REFERENCES `method_payment` (`pan`) ON UPDATE CASCADE
-)
+);
 
 
-DROP TABLE IF EXISTS address;
+
 
 CREATE TABLE `address` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`name`)
-)
+);
 
-DROP TABLE IF EXISTS registereduser_has_address;
+
 
 CREATE TABLE `registereduser_has_address` (
   `id_has_address` int AUTO_INCREMENT PRIMARY KEY,
@@ -151,10 +147,7 @@ CREATE TABLE `registereduser_has_address` (
   KEY `nameAddress_HasAddress_idx` (`nameAddress`),
   CONSTRAINT `usernameRegisteredUser_HasAddress` FOREIGN KEY (`usernameRegisteredUser`) REFERENCES `registereduser` (`username`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `nameAddress_HasAddress` FOREIGN KEY (`nameAddress`) REFERENCES `address` (`name`) ON DELETE RESTRICT ON UPDATE CASCADE
-) 
-
-
-
+) ;
 
 
 
