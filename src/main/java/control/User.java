f7ea7e9ea5f03175df4fbf6ccb3175111c430162
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-30 17:35:59 +0200

    Update User.java

--- src/main/java/control/User.java
+++ src/main/java/control/User.java
@@ -1,12 +1,16 @@
 package control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
+import dao.RegisteredUserDaoDataSource;
+import bean.RegisteredUserBean;
 /**
  * Servlet implementation class User
  */
@@ -26,8 +30,39 @@ public class User extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String errors="";
+	    
+	Boolean isRegisteredUser=(Boolean)request.getSession().getAttribute("isRegisteredUser");
+	if (Boolean.FALSE.equals(isRegisteredUser)) {
+	    errors += "Non sei utente<br>";
+	    request.setAttribute("errors", errors);
+	    request.getRequestDispatcher("/view/index.jsp").forward(request, response);
+	    return;
+	}
+	
+	
+		String username=(String)request.getSession().getAttribute("username");
+		RequestDispatcher dispatchToIndexPage=request.getRequestDispatcher("/view/index.jsp");
+		RequestDispatcher dispatchToUserPage=request.getRequestDispatcher("/view/user.jsp");
+		RegisteredUserBean user=new RegisteredUserBean();
+		RegisteredUserDaoDataSource ds=new RegisteredUserDaoDataSource();
+		try {
+			
+			user=ds.doRetrieveByKey(username);
+			
+			
+			
+		}
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+			dispatchToIndexPage.forward(request, response);
+		}
+		
+		request.setAttribute("user", user);
+		dispatchToUserPage.forward(request, response);
+		
 	}
 
 	/**
