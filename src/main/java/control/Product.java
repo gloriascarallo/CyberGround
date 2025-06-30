b0de9cb324e877bd5f4e68c5c44892f58570c9c7
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-06-30 17:38:34 +0200

    Update Product.java

--- src/main/java/control/Product.java
+++ src/main/java/control/Product.java
@@ -6,12 +6,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
+import dao.ProductDaoDataSource;
+import bean.ProductBean;
 /**
  * Servlet implementation class Product
  */
 @WebServlet("/Product")
-public class Product extends HttpServlet {
+public class Product extends HttpServlet {  
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -26,15 +29,29 @@ public class Product extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int id=Integer.parseInt(request.getParameter("idProdotto"));
+		
+		ProductDaoDataSource ds=new ProductDaoDataSource();
+		ProductBean product=new ProductBean();
+		try {product=ds.doRetrieveByKey(id);
+		}
+		
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("product", product);
+		request.getRequestDispatcher("/view/product.jsp").forward(request, response);
+		return;
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
