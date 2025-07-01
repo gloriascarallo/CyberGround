a6257b229f40e802540610f3a0d1e31c3ac8348c
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-07-01 11:55:28 +0200

    Update Refund.java

--- src/main/java/control/Refund.java
+++ src/main/java/control/Refund.java
@@ -6,7 +6,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.sql.SQLException;
+import java.util.ArrayList;
+import bean.OrderBean;
+import bean.Product_in_orderBean;
+import dao.OrderDaoDataSource;
+import dao.Product_in_orderDaoDataSource;
 /**
  * Servlet implementation class Refund
  */
@@ -26,8 +31,53 @@ public class Refund extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String errors="";
+		
+		Boolean isRegisteredUser=(Boolean)request.getSession().getAttribute("isRegisteredUser");
+		if (Boolean.FALSE.equals(isRegisteredUser)) {
+		    errors += "Non sei utente registrato<br>";
+		    request.setAttribute("errors", errors);
+		    request.getRequestDispatcher("/view/refund.jsp").forward(request, response);
+		    return;
+		}
+		
+		int id=(Integer)(request.getAttribute("id"));
+		OrderDaoDataSource ds_order=new OrderDaoDataSource();
+		OrderBean order=null;
+		
+		try {
+			
+			order=ds_order.doRetrieveByKey(id);
+			
+			if(order==null) {
+				
+				errors+="Ordine non trovato<br>";
+				request.setAttribute("errors", errors);
+				request.getRequestDispatcher("/view/refund.jsp").forward(request, response);
+				return;
+				
+			}
+			
+				Product_in_orderDaoDataSource ds_products=new Product_in_orderDaoDataSource();
+				ArrayList<Product_in_orderBean> products=ds_products.doRetrieveByIdOrder(order.getIdOrder());
+				order.setProducts_in_order(products);			
+				request.setAttribute("order", order);
+				request.getRequestDispatcher("/view/order_refund.jsp").forward(request, response);
+				return;
+						
+						
+					
+				
+			}
+			
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 	/**
