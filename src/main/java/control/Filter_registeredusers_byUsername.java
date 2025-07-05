ecab5b740b4922e22bd50c0c9f3d1a7cccbd2ddf
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-07-05 19:27:32 +0200

    Create Filter_registeredusers_byUsername.java

---
+++ src/main/java/control/Filter_registeredusers_byUsername.java
@@ -0,0 +1,61 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import bean.RegisteredUserBean;
+import dao.RegisteredUserDaoDataSource;
+/**
+ * Servlet implementation class Filter_registeredusers_byUsername
+ */
+@WebServlet("/Filter_registeredusers_byUsername")
+public class Filter_registeredusers_byUsername extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Filter_registeredusers_byUsername() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		RegisteredUserDaoDataSource ds=new RegisteredUserDaoDataSource();
+		RegisteredUserBean user=new RegisteredUserBean();
+		try {
+			
+			user=ds.doRetrieveByKey(username);
+			
+		}
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		request.setAttribute("user", user);
+		request.getRequestDispatcher("/admin/").forward(request, response); // da finire
+		return;
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
