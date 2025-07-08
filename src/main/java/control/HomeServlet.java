a9c94e1fa60fc0ac303dff228a06dfe3a62a1d67
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-07-08 11:32:23 +0200

    guestId cookie

--- src/main/java/control/HomeServlet.java
+++ src/main/java/control/HomeServlet.java
@@ -2,6 +2,7 @@ package control;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -23,7 +24,7 @@ import javax.sql.DataSource;
 @WebServlet("/HomeServlet")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	public static final String SECRET_KEY = "qwerTY-SECRET-KEY-2025";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -50,8 +51,36 @@ public class HomeServlet extends HttpServlet {
 				System.out.println("Error:" + e.getMessage());
 			}
 		
-	
-		if(request.getSession().getAttribute("id")==null) {
+			Cookie[] cookies = request.getCookies();
+			String guestIdStr = null;
+			int guestId=-1;
+
+			if (cookies != null) {
+			    for (Cookie c : cookies) {
+			        if ("guestId".equals(c.getName())) {
+			            guestIdStr = c.getValue(); // es. "42|abc123hash"
+		                String[] parts = guestIdStr.split("\\|");
+
+		                if (parts.length == 2) {
+		                    try {
+		                    	int parsedGuestId = Integer.parseInt(parts[0]);
+		                        String expectedHash = Security.hmacSHA256(String.valueOf(parsedGuestId), SECRET_KEY);
+		                        if (expectedHash.equals(parts[1])) {
+		                        	guestId=parsedGuestId;
+		                            break; 
+		                        } else {
+		                            System.out.println("Hash mismatch - possibile manipolazione!");
+		                        }
+		                    } catch (NumberFormatException e) {
+		                        System.out.println("guestId non è un intero valido.");
+		                    }
+			          
+			        }
+			    }
+			  }
+			}
+			
+		if(guestId==-1) {
 			
 			 try (Connection con = ds.getConnection()) {
 			        PreparedStatement ps = con.prepareStatement(
@@ -63,26 +92,33 @@ public class HomeServlet extends HttpServlet {
 
 			        ResultSet rs = ps.getGeneratedKeys();
 			        if (rs.next()) {
-			            int clientId = rs.getInt(1);
-			            request.getSession().setAttribute("id", clientId);
+			            guestId = rs.getInt(1);
+			            request.getSession().setAttribute("id", guestId);
 			            PreparedStatement psCart = con.prepareStatement(
 				                "INSERT INTO CART (IDCART) VALUES (?)"
 				            );
-				            psCart.setInt(1, clientId);
+				            psCart.setInt(1, guestId);
 				            psCart.executeUpdate();
 				            psCart.close();
 
 				            CartBean cart = new CartBean();
-				            cart.setIdCart(clientId);
+				            cart.setIdCart(guestId);
 				            request.getSession().setAttribute("cart", cart);
 				        }
 				        ps.close();
 			        }
 			     catch (SQLException e) {
 			        e.printStackTrace(); 
+			        request.getRequestDispatcher("/500.html").forward(request, response);
+			        return;
 			    }
 		}
-			
+		String value = guestId + "|" + Security.hmacSHA256(String.valueOf(guestId), SECRET_KEY);
+		Cookie guestCookie = new Cookie("guestId", value);
+	    guestCookie.setMaxAge(60 * 60 * 24 * 30); 
+	    guestCookie.setPath("/");
+	    guestCookie.setHttpOnly(true);
+	    response.addCookie(guestCookie);
 		request.getSession().setAttribute("isAdmin", Boolean.FALSE);
 		request.getSession().setAttribute("isRegisteredUser", Boolean.FALSE);
 		response.sendRedirect(request.getContextPath()+"/view/index.jsp");
@@ -90,7 +126,6 @@ public class HomeServlet extends HttpServlet {
 			
 		}
 		
-	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
