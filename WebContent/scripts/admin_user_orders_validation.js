f82796068fd579f9738568c29859418922c204a4
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-07-16 14:17:49 +0200

    Update admin_user_orders_validation.js

--- WebContent/scripts/admin_user_orders_validation.js
+++ WebContent/scripts/admin_user_orders_validation.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("orderDateForm");
   if (!form) return;
 
